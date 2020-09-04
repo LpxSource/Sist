d2fa7b2bb2278e1e4e7a40da889a49244da896fe
LpxSource <igorgabrielsb090@gmail.com>
2020-09-03 23:54:37 -0300

    LP2

--- LP2/Módulo 3 - Tarefa 1.c
+++ LP2/Módulo 3 - Tarefa 1.c
@@ -1,14 +1,4 @@
-/*Fa�a um programa que armazene os dados de clientes da Loja Gama. Os dados a serem armazenados s�o:
->nome
->endere�o
->telefone
->renda m�dia trimestral
->cr�dito para compras no credi�rio
-Para tanto, dever� ser lido do usu�rio nome, endere�o, telefone e os tr�s �ltimos sal�rios. O programa dever� calcular:
-a renda m�dia trimestral;
-cr�dito para compras no credi�rio (30% da renda m�dia)
-Os dados dever�o ser armazenados em structs que dever�o ser alocadas de forma din�mica.
-O programa dever� imprimir os dados armazenados dos clientes.*/
+
 #include<locale.h>
 #include<stdio.h>
 #include<stdlib.h>
@@ -33,11 +23,11 @@ int main(){
 		setbuf(stdin,NULL);
 		fgets(c[j].endereco,50,stdin);
 		printf("Informe seus tres ultimos salarios\n");
-		printf("1� Salario:\n");
+		printf("1° Salario:\n");
 		scanf("%f",&c[j].salario1);
-		printf("2� Salario:\n");
+		printf("2° Salario:\n");
 		scanf("%f",&c[j].salario2);
-		printf("3� Salario:\n");
+		printf("3° Salario:\n");
 		scanf("%f",&c[j].salario3);
 		printf("Deseja fazer um novo cadastro? [s/n]");
 		setbuf(stdin,NULL);
@@ -54,9 +44,9 @@ int main(){
 		c[b].rendatrime=(c[b].salario1+c[b].salario2+c[b].salario3)/3;
 		c[b].credito=(c[b].rendatrime*0.3);
 		printf("Nome:%s\n",c[a].nome);
-		printf("Endere�o:%s\n",c[a].endereco);
-		printf("M�dia Trimestral:%.2f\n",c[a].rendatrime);
-		printf("Cr�dito:%.2f",c[a].credito);
+		printf("Endereco:%s\n",c[a].endereco);
+		printf("Media Trimestral:%.2f\n",c[a].rendatrime);
+		printf("Credito:%.2f",c[a].credito);
 	}
     free(c);
     return 0;
